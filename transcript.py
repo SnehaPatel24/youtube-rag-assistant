8cb42ed0818569307a1492f5a5923276d10cdc5d
Sneha Patel <itzsnehapatel2004@gmail.com>
2026-07-18 17:15:04 +0530

    edited transcript.py

--- transcript.py
+++ transcript.py
@@ -1,37 +1,15 @@
 from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api._errors import TranscriptsDisabled
-from urllib.parse import urlparse, parse_qs
+import streamlit as st
 
 
-def extract_video_id(url):
-
-    if "youtu.be" in url:
-        return url.split("/")[-1]
-
-    parsed = urlparse(url)
-
-    if parsed.hostname in ["www.youtube.com", "youtube.com"]:
-        return parse_qs(parsed.query)["v"][0]
-
-    return url
-
-
-def get_transcript(url):
-
-    video_id = extract_video_id(url)
-
+def get_transcript(video_id):
     try:
-        yt = YouTubeTranscriptApi()
+        api = YouTubeTranscriptApi()
 
-        transcript = yt.fetch(video_id)
+        transcript = api.fetch(video_id)
 
-        text = " ".join(chunk.text for chunk in transcript)
-
-        return text
-
-    except TranscriptsDisabled:
-        return None
+        return " ".join(item.text for item in transcript)
 
     except Exception as e:
-        print(e)
-        return None
+        st.exception(e)
+        raise
